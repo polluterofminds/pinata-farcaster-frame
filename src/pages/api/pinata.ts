d02a2f6d239e9bacfba6002683b4a45720fc0054
polluterofminds <justin.edward.hunter@gmail.com>
2024-01-26 16:28:48 -0600

    animated

--- src/pages/api/pinata.ts
+++ src/pages/api/pinata.ts
@@ -11,7 +11,7 @@ const data = `
     <meta property="fc:frame:image" content="https://mktg.mypinata.cloud/ipfs/Qmbdi1gDbXPZx74N8ccUZ2kpF3Kqwk53zYXvT31d7MPb7T?filename=pinnie.png" />
     <meta property="fc:frame:button:1" content="Nothing to see here [TEST]" />
     <meta property="fc:frame" content="vNext" />
-    <title>Pinata Discount</title>
+    <title>Pinnie takes flight</title>
   </head>
   <body>
     <img src="https://mktg.mypinata.cloud/ipfs/Qmbdi1gDbXPZx74N8ccUZ2kpF3Kqwk53zYXvT31d7MPb7T?filename=pinnie.png" />
@@ -33,14 +33,43 @@ export default function handler(
   } else if (req.method === "POST") {
     try {
       console.log(req.body);
+      if(req.body.untrustedData.buttonIndex === 1) {
+        const resultData = `
+        <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="https://mktg.mypinata.cloud/ipfs/QmbUziAUSh68xmCKrUozYoYycbS3wcfrYsuiuTY9Zfd3WS?filename=pinnie.gif" />
+      <body>
+      <img src="https://mktg.mypinata.cloud/ipfs/QmbUziAUSh68xmCKrUozYoYycbS3wcfrYsuiuTY9Zfd3WS?filename=pinnie.gif" />
+    </body>
+  </html>`
+        return res.send(resultData);
+      } else {
+        const resultData = `
+        <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <meta property="fc:frame:button:1" content="Wrong answer, try again" />
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="https://mktg.mypinata.cloud/ipfs/Qmbdi1gDbXPZx74N8ccUZ2kpF3Kqwk53zYXvT31d7MPb7T?filename=pinnie.png" />
+      <body>
+      <img src="https://mktg.mypinata.cloud/ipfs/Qmbdi1gDbXPZx74N8ccUZ2kpF3Kqwk53zYXvT31d7MPb7T?filename=pinnie.png" />
+    </body>
+  </html>`
+        return res.send(resultData);
+      }
       //  Verify signature
 
       //  Generate image from Dalle
       //  Upload to Pinata
       //  Return results with img url
-      const resultData = `<meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://mktg.mypinata.cloud/ipfs/Qmbdi1gDbXPZx74N8ccUZ2kpF3Kqwk53zYXvT31d7MPb7T?filename=pinnie.png" />`
-      res.send(resultData);
     } catch (error) {
       res.send("Server error")
     }
